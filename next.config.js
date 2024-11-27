b392257203ae85489b5863a8d12020a8a2b50bc5
kidMax25 <snmaxis@gmail.com>
2024-11-27 22:58:19 +0300

    fix: convert next config to js file

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // swcMinify is no longer needed as it's the default in Next.js 14
+}
+
+module.exports = nextConfig
